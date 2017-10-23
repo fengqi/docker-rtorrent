f50b3990abc97a2d24716b85257a80f027ff643f
fengqi <lyf362345@gmail.com>
2017-10-23 12:37:42 +0800

    配置调整

--- src/conf/rutorrent.php
+++ src/conf/rutorrent.php
@@ -27,21 +27,21 @@
 	$topDirectory = '/app';			// Upper available directory. Absolute path with trail slash.
 	$forbidUserSettings = false;
 
-	$scgi_port = 6000;
-	$scgi_host = "127.0.0.1";
+	//$scgi_port = 6000;
+	//$scgi_host = "127.0.0.1";
 
 	// For web->rtorrent link through unix domain socket
 	// (scgi_local in rtorrent conf file), change variables
 	// above to something like this:
 	//
-	// $scgi_port = 0;
-	// $scgi_host = "unix:///tmp/rpc.socket";
+	$scgi_port = 0;
+	$scgi_host = "unix:///run/rtorrent.sock";
 
 	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
 
 	$pathToExternals = array(
 		"php" 	=> '/usr/bin/php',			// Something like /usr/bin/php. If empty, will be found in PATH.
-		"curl"	=> '/usr/bin/curl',			// Something like /usr/bin/curl. If empty, will be found in PATH.
+		"curl"	=> '/usr/local/bin/curl',	// Something like /usr/bin/curl. If empty, will be found in PATH.
 		"gzip"	=> '/bin/gzip',				// Something like /usr/bin/gzip. If empty, will be found in PATH.
 		"id"	=> '/usr/bin/id',			// Something like /usr/bin/id. If empty, will be found in PATH.
 		"stat"	=> '/usr/bin/stat',			// Something like /usr/bin/stat. If empty, will be found in PATH.
